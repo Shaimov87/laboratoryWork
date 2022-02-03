f6213e995cd4ba4d8aa2d2eaa212d519e28a678b
Shaimov87 <shaimov87@mail.ru>
2022-02-03 16:59:52 +0700

    Add project files.

---
+++ Money.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laboratoryWork
+{
+    class Money
+    {
+        private string currencyCode;
+        public double amount;
+
+        public Money()
+        {
+            currencyCode = null;
+            amount = 0;
+        }
+
+        public Money(string currencyCode,double amount)
+        {
+            this.currencyCode = currencyCode;
+            this.amount = amount;
+        }
+        public string CurrencyCode
+        {
+            get { return currencyCode; }
+            set { this.currencyCode = currencyCode; }
+        }
+        public double Amount
+        {
+            get { return amount; }
+            set { this.amount = amount; }
+        }
+        public override string ToString()
+        {
+            return $"\tYour balance = {amount} {currencyCode}\n";
+        }
+        //public  delegate double MoneyConvertDelegate(double value);
+
+        public void convertMoney(MoneyConvertDelegate all_convert)
+        {
+            //Console.WriteLine("please enter amount");
+            //double a = Convert.ToDouble(Console.ReadLine());
+            foreach(MoneyConvertDelegate operation in all_convert.GetInvocationList())
+            {
+                Console.WriteLine($"result convert {operation(amount)}");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laboratoryWork
+{
+    public delegate double MoneyConvertDelegate(double value);
+    class Program
+    {
+        //public delegate double MoneyConvertDelegate(double value);
+        static void Main(string[] args)
+        {
+            Money money=new Money("dollar",200);
+            MoneyConvertDelegate convert = null;
+            convert += delegate (double amount)
+             {
+                 Console.WriteLine($"{ amount}$ = { amount * 75} Rub");
+                 return amount;
+             };
+            convert += delegate (double amount)
+            {
+                Console.WriteLine($"{ amount}$ = { amount / 1.13} Euro");
+                return amount;
+            };
+            convert += delegate (double amount)
+            {
+                Console.WriteLine($"{ amount}$ = { amount * 0.0087} Yen");
+                return amount;
+            };
+            convert += (double amount) =>
+              {
+                  Console.WriteLine($"{ amount}$ = { amount / 1809.34} Oz Gold");
+                  return amount;
+              };
+            money.convertMoney(convert);
+        }
+    }
+}
